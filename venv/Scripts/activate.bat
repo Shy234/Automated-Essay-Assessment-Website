aebd065132b52a78db840bb4a9f9234f7ebfade0
Shy234 <ldacilloshaina@gmail.com>
2024-02-16 20:23:21 +0800

    update

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\lunaj\OneDrive\Desktop\AES\venv
+set VIRTUAL_ENV=C:\Users\ADMIN\Desktop\Automated-Essay-Assessment-Website\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
